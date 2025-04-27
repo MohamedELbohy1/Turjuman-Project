dcd06e2792df4448b009b79e613adb4dd7454f40
MohamedELbohy1 <elbohym33@gmail.com>
2025-04-27 20:29:39 +0200

    Token-v2

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -113,13 +113,12 @@ exports.protectUserTranslate = asyncHandler(async (req, res, next) => {
 
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
-  // if (
-  //   req.headers.authorization &&
-  //   req.headers.authorization.startsWith("Bearer")
-  // ) {
-  //   token = req.headers.authorization.split(" ")[1];
-  //}
-  if (req.cookies.jwt) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }
 
