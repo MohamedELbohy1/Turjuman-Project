59f629995a475e3c88825a8e4794a0222c6f6233
MohamedELbohy1 <elbohym33@gmail.com>
2025-04-27 20:28:46 +0200

    token

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -75,13 +75,12 @@ exports.logout = (req, res) => {
 /// Protect Routes /////
 exports.protectUserTranslate = asyncHandler(async (req, res, next) => {
   let token;
-  // if (
-  //   req.headers.authorization &&
-  //   req.headers.authorization.startsWith("Bearer")
-  // ) {
-  //   token = req.headers.authorization.split(" ")[1];
-  // }
-  if (req.cookies.jwt) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }
 
