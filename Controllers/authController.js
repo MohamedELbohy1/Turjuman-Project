129c8a73fdacfd2b2b17f05cd4c01d1f17f7b2da
MohamedELbohy1 <elbohym33@gmail.com>
2025-04-28 02:01:36 +0200

    testing-v5

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -11,18 +11,30 @@ const signToken = (id) => {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, res, req) => {
   const token = signToken(user._id);
+
   const cookieOptions = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() +
+        (process.env.JWT_COOKIE_EXPIRES_IN || 90) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure:
+      process.env.NODE_ENV === "production" ||
+      req.hostname.includes("vercel.app"),
+    sameSite:
+      process.env.NODE_ENV === "production" ||
+      req.hostname.includes("vercel.app")
+        ? "None"
+        : "Lax",
+    path: "/",
   };
-  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
   res.cookie("jwt", token, cookieOptions);
 
   user.password = undefined;
+
   res.status(statusCode).json({
     status: "success",
     token,
